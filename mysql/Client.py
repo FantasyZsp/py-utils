a359504bf4fafaef71d2b34b561d1d1df306cf3d
zhaoshipan <541054738@qq.com>
2019-12-25 17:24:23 +0800

    fix

--- mysql/Client.py
+++ mysql/Client.py
@@ -1,16 +1,17 @@
 # batch_generate_yml_file()
 #
 from mysql.DBUtils import DbInfo
-from mysql.EtlUtils import batch_default
+from mysql.EtlUtils import batch_default, batch_test
 
 username = "canal"  # 用户名
 password = "canal"  # 连接密码
 ip = "192.168.2.116"  # 连接地址
 port = 13306  # 连接端口
-database = "avengers"  # 数据库名
+database = "avengers_test"  # 数据库名
 
 databaseInfo = DbInfo.build(username, password, ip, port, database)
 # 获取表名
-tables = databaseInfo.list_table(databaseInfo)
+tables = databaseInfo.list_table()
 # 同步
-batch_default(tables)
+# batch_default(tables)
+batch_test(tables)
