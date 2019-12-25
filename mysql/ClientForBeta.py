1f34369607ace9082fb187f903d12167165a89e9
zhaoshipan <541054738@qq.com>
2019-12-25 17:26:00 +0800

    reactor

---
+++ mysql/ClientForBeta.py
@@ -0,0 +1,16 @@
+# batch_generate_yml_file()
+#
+from mysql.DBUtils import DbInfo
+from mysql.EtlUtils import batch_default
+
+username = "canal"  # 用户名
+password = "canal"  # 连接密码
+ip = "192.168.2.116"  # 连接地址
+port = 13306  # 连接端口
+database = "avengers"  # 数据库名
+
+databaseInfo = DbInfo.build(username, password, ip, port, database)
+# 获取表名
+tables = databaseInfo.list_table()
+# 同步
+batch_default(tables)
